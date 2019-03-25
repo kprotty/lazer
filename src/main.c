a37967fe201c030562485a87ca7979c97a055399
kprotty <kbutcher6200@gmail.com>
2019-03-25 14:21:43 -0500

    Rewrote heap allocation and freeing

--- src/main.c
+++ src/main.c
@@ -1,5 +1,7 @@
-
+#include "runtime/memory/heap.h"
 
 int main(int argc, const char* argv[]) {
+    lzr_heap_init();
+
     return 0;
 }
--- src/runtime/memory/heap.h
+++ src/runtime/memory/heap.h
@@ -3,14 +3,14 @@
 
 #include "memory.h"
 
-#define LZR_HEAP_START      (1ULL << 35)
+#define LZR_HEAP_BEGIN      (1ULL << 35)
 #define LZR_HEAP_END        (1ULL << 36)
 #define LZR_HEAP_CHUNK_SIZE (2 * 1024 * 1024)
 
 void lzr_heap_init();
 
-void lzr_heap_free(void* ptr);
+void* lzr_heap_alloc(uint16_t num_chunks);
 
-void* lzr_heap_alloc(size_t num_chunks);
+void lzr_heap_free(void* ptr);
 
 #endif // LZR_HEAP_H
