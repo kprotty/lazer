fdf0d1b8c598f096f928af80d9cecb5caabc664e
king1600 <3dsarcard@gmail.com>
2019-04-10 20:33:18 -0500

    Spinlock reschedule thread during contention

--- src/runtime/os/lock.c
+++ src/runtime/os/lock.c
@@ -2,11 +2,14 @@
 
 #if defined(LZR_WINDOWS)
     #include <Windows.h>
+    #define cpu_yield() Sleep(1)
     #define cpu_relax() YieldProcessor()
 
 #elif defined(LZR_LINUX)
+    #include <sched.h>
     #include <immintrin.h>
     #define cpu_relax() _mm_pause()
+    #define cpu_yield() sched_yield()
 #endif
 
 void lzr_spinlock_init(lzr_spinlock_t* self) {
@@ -14,8 +17,15 @@ void lzr_spinlock_init(lzr_spinlock_t* self) {
 }
 
 void lzr_spinlock_lock(lzr_spinlock_t* self) {
-    while (!atomic_flag_test_and_set_explicit(self, memory_order_relaxed))
-        cpu_relax();
+    size_t contended = 0;
+    while (!atomic_flag_test_and_set_explicit(self, memory_order_relaxed)) {
+        if (++contended > 20) {
+            cpu_relax();
+        } else {
+            cpu_yield();
+        }
+    }
+
     atomic_thread_fence(memory_order_release);
 }
 
