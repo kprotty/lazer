6c5011e28ae515064eedd9476b120ca275d0de18
king1600 <3dsarcard@gmail.com>
2019-04-19 20:15:18 -0500

    Simplify memory api & tests

--- src/os/memory.zig
+++ src/os/memory.zig
@@ -1,64 +1,51 @@
+const std = @import("std");
 const builtin = @import("builtin");
-const linux = @import("std").os.linux;
-const onWindows = builtin.os == builtin.Os.windows;
 
-const MADV_WILLNEED = 3;
+const linux = std.os.linux;
+const windows = builtin.os == builtin.Os.windows;
+
 const MADV_DONTNEED = 4;
-
 const MEM_COMMIT = 0x1000;
 const MEM_RESERVE = 0x2000;
 const MEM_DECOMMIT = 0x4000;
 const PAGE_READWRITE = 0x04;
+const INVALID = std.math.maxInt(usize);
 
-extern "kernel32" stdcallcc fn VirtualFree(address: usize, bytes: usize, freeType: u32) u32;
-extern "kernel32" stdcallcc fn VirtualAlloc(address: usize, bytes: usize, allocType: u32, protect: u32) usize;
+extern "kernel32" stdcallcc fn VirtualFree(address: usize, bytes: usize, free_type: u32) bool;
+extern "kernel32" stdcallcc fn VirtualAlloc(address: usize, bytes: usize, alloc_type: u32, protect: u32) usize;
 
-pub const MapError = error {
-    MapFailed,
-    CommitFailed,
-    DecommitFailed,
-};
+pub fn map(address: usize, bytes: usize) usize {
+    if (windows)
+        return VirtualAlloc(address, bytes, MEM_RESERVE, PAGE_READWRITE);
 
-pub fn map(comptime T: type, address: usize, amount: usize) ![*]T {
-    const bytes = @sizeOf(T) * amount;
-    const protect = linux.PROT_READ | linux.PROT_WRITE;
+    const prot = linux.PROT_READ | linux.PROT_WRITE;
     const flags = linux.MAP_FIXED | linux.MAP_PRIVATE | linux.MAP_ANONYMOUS;
+    const addr = linux.syscall6(linux.SYS_mmap, address, bytes, prot, flags, INVALID, 0);
 
-    const addr =
-        if (onWindows) VirtualAlloc(address, bytes, MEM_RESERVE, PAGE_READWRITE)
-        else linux.syscall6(linux.SYS_mmap, address, bytes, protect, flags, -1, 0);
-
-    return
-        if (addr == address) @intToPtr([*]T, addr)
-        else MapError.MapFailed;
+    return if (addr == address) address else 0;
 }
 
-pub fn commit(comptime T: type, address: [*]T, amount: usize) !void {
-    const addr = @ptrToInt(address);
-    const bytes = @sizeOf(T) * amount;
+pub fn commit(address: usize, bytes: usize) bool {
+    if (windows)
+        return VirtualAlloc(address, bytes, MEM_COMMIT, PAGE_READWRITE) == address;
     
-    if (
-        if (onWindows) VirtualAlloc(addr, bytes, MEM_COMMIT, PAGE_READWRITE) != addr
-        else false // linux.syscall4(linux.SYS_madvise, addr, bytes, MADV_WILLNEED) != 0
-    ) return MapError.CommitFailed;
+    return true; // linux overcommits by default
 }
 
-pub fn decommit(comptime T: type, address: [*]T, amount: usize) !void {
-    const addr = @ptrToInt(address);
-    const bytes = @sizeOf(T) * amount;
-
-    if (
-        if (onWindows) VirtualFree(addr, bytes, MEM_DECOMMIT) == 0
-        else linux.syscall3(linux.SYS_madvise, addr, bytes, MADV_DONTNEED) != 0
-    ) return MapError.DecommitFailed;
+pub fn decommit(address: usize, bytes: usize) bool {
+    if (windows)
+        return VirtualFree(address, bytes, MEM_DECOMMIT) != 0;
+    
+    return linux.syscall3(linux.SYS_madvise, address, bytes, MADV_DONTNEED) == 0;
 }
 
 test "memory mapping" {
     const assert = @import("std").debug.assert;
-    const memory = try map(u8, 1 << 32, 4096);
+    const memory = @intToPtr([*]u8, map(1 << 32, 4096));
+    
+    assert(commit(@ptrToInt(memory), 4096));
+    defer assert(decommit(@ptrToInt(memory), 4096));
 
-    try commit(u8, memory, 4096);
     memory[0] = 0xff;
     assert(memory[0] == 0xff);
-    try decommit(u8, memory, 4096);
 }
