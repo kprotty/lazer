65429a715b613825924cd3df7de4399c8acb745f
kprotty <kbutcher6200@gmail.com>
2019-04-29 12:15:34 -0500

    Resetting all + cleanup memory api

--- src/platform/memory.rs
+++ src/platform/memory.rs
@@ -2,98 +2,104 @@ use core::mem::size_of;
 use core::num::NonZeroUsize;
 use core::slice::from_raw_parts_mut;
 
-pub fn map<T: Sized>(address: usize, amount: usize, commit: bool) -> Option<&'static mut [T]> {
-    self::memory
-        ::map(address, size_of::<T>() * amount, commit)
-        .map(|ptr| unsafe { from_raw_parts_mut(ptr.get() as *mut T, amount) })
+pub fn map<T: Sized>(address: usize, amount: usize) -> Option<&'static mut[T]> {
+    unsafe {
+        self::memory
+            ::map(address, size_of::<T>() * amount)
+            .map(|ptr| from_raw_parts_mut(ptr.get() as *mut _, amount))
+    }
 }
 
-pub fn commit<T: Sized>(address_range: &[T], executable: bool) -> Option<()> {
-    self::memory::commit(
-        address_range.as_ptr() as usize,
-        address_range.len() * size_of::<T>(),
-        executable,
-    )
+pub fn unmap<T: Sized>(memory: &[T]) -> Option<()> {
+    unsafe { 
+        self::memory::unmap(
+            memory.as_ptr() as usize,
+            memory.len() * size_of::<T>()
+        )
+    }
 }
 
-pub fn decommit<T: Sized>(address_range: &[T]) -> Option<()> {
-    self::memory::decommit(
-        address_range.as_ptr() as usize,
-        address_range.len() * size_of::<T>(),
-    )
+pub fn commit<T: Sized>(memory: &[T]) -> Option<()> {
+    unsafe {
+        self::memory::commit(
+            memory.as_ptr() as usize,
+            memory.len() * size_of::<T>()
+        )
+    }
 }
 
-#[cfg(target_os = "linux")]
+pub fn decommit<T: Sized>(memory: &[T]) -> Option<()> {
+    unsafe {
+        self::memory::decommit(
+            memory.as_ptr() as usize,
+            memory.len() * size_of::<T>()
+        )
+    }
+}
+
+#[cfg(unix)]
 mod memory {
+    use libc::*;
     use super::NonZeroUsize;
-    use libc::{
-        mmap, madvise,
-        MADV_DONTNEED,
-        PROT_READ, PROT_WRITE, PROT_EXEC,
-        MAP_PRIVATE, MAP_ANONYMOUS, MAP_FIXED, MAP_FAILED,
-    };
 
-    pub fn map(address: usize, bytes: usize, _commit: bool) -> Option<NonZeroUsize> {
-        let flags = MAP_PRIVATE | MAP_ANONYMOUS | if address > 0 { MAP_FIXED } else { 0 };
-        
-        match unsafe { mmap(address as *mut _, bytes, PROT_READ | PROT_WRITE, flags, -1, 0) } {
-            MAP_FAILED => None,
-            ptr if ptr as usize != address => None,
-            ptr => Some(unsafe { NonZeroUsize::new_unchecked(ptr as usize) })
+    pub unsafe fn map(address: usize, bytes: usize) -> Option<NonZeroUsize> {
+        let prot = PROT_READ | PROT_WRITE;
+        let flags = MAP_FIXED | MAP_PRIVATE | MAP_ANONYMOUS;
+        match mmap(address as *mut _, bytes, prot, flags, -1, 0) as usize {
+            ptr if ptr != address => None,
+            ptr => Some(NonZeroUsize::new_unchecked(ptr)),
         }
     }
 
-    pub fn commit(address: usize, bytes: usize, executable: bool) -> Option<()> {
-        // linux over-commits by default so madvise call not required
-        let flags = PROT_READ | PROT_WRITE | PROT_EXEC;
-
-        if executable && unsafe { mprotect(address as *mut _, bytes, flags) == 0 } {
-            Some(())
-        } else {
-            None
+    pub unsafe fn unmap(address: usize, bytes: usize) -> Option<()> {
+        match munmap(address as *mut _, bytes) {
+            0 => None,
+            _ => Some(()),
         }
     }
 
-    pub fn decommit(address: usize, bytes: usize) -> Option<()> {
-        match unsafe { madvise(address as *mut _, bytes, MADV_DONTNEED) } {
+    pub unsafe fn commit(address: usize, bytes: usize) -> Option<()> {
+        // linux over-commits by default
+        Some(())
+    }
+
+    pub unsafe fn decommit(address: usize, bytes: usize) -> Option<()> {
+        match madvise(address as *mut _, bytes, MADV_DONTNEED) {
             0 => Some(()),
             _ => None,
         }
     }
 }
 
-#[cfg(target_os = "windows")]
+#[cfg(windows)]
 mod memory {
     use super::NonZeroUsize;
-    use winapi::shared::minwindef::TRUE;
-    use winapi::um::{
-        memoryapi::{VirtualAlloc, VirtualFree},
-        winnt::{
-            MEM_COMMIT, MEM_DECOMMIT, MEM_RESERVE,
-            PAGE_READWRITE, PAGE_EXECUTE_READWRITE,
-        },
-    };
+    use winapi::shared::minwindef::*;
+    use winapi::um::{winnt::*, memoryapi::*};
 
-    pub fn map(address: usize, bytes: usize, commit: bool) -> Option<NonZeroUsize> {
-        let alloc_type = MEM_RESERVE | if commit { MEM_COMMIT } else { 0 };
-        
-        match unsafe { VirtualAlloc(address as *mut _, bytes, alloc_type, PAGE_READWRITE) } {
-            ptr if ptr.is_null() || ptr as usize != address => None,
-            ptr => Some(unsafe { NonZeroUsize::new_unchecked(ptr as usize) })
+    pub unsafe fn map(address: usize, bytes: usize) -> Option<NonZeroUsize> {
+        match VirtualAlloc(address as *mut _, bytes, MEM_RESERVE, PAGE_READWRITE) as usize {
+            0 => None,
+            ptr => Some(NonZeroUsize::new_unchecked(ptr)),
         }
     }
 
-    pub fn commit(address: usize, bytes: usize, executable: bool) -> Option<()> {
-        let protect = if executable { PAGE_EXECUTE_READWRITE } else { PAGE_READWRITE };
-
-        match unsafe { VirtualAlloc(address as *mut _, bytes, MEM_COMMIT, protect) } {
-            ptr if ptr as usize == address => Some(()),
+    pub unsafe fn unmap(address: usize, bytes: usize) -> Option<()> {
+        match VirtualFree(address as *mut _, 0, MEM_RELEASE) {
+            TRUE => Some(()),
             _ => None,
         }
     }
 
-    pub fn decommit(address: usize, bytes: usize) -> Option<()> {
-        match unsafe { VirtualFree(address as *mut _, bytes, MEM_DECOMMIT) } {
+    pub unsafe fn commit(address: usize, bytes: usize) -> Option<()> {
+        match VirtualAlloc(address as *mut _, bytes, MEM_COMMIT, PAGE_READWRITE) {
+            ptr if ptr.is_null() => None,
+            _ => Some(()),
+        }
+    }
+
+    pub unsafe fn decommit(address: usize, bytes: usize) -> Option<()> {
+        match VirtualFree(address as *mut _, bytes, MEM_DECOMMIT) {
             TRUE => Some(()),
             _ => None,
         }
